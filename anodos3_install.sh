8a5dbdb54375893f6ea37c5415c4474f3d5ce4bd
HashTalt <41134069+HashTalt@users.noreply.github.com>
2018-08-03 20:45:01 +0300

    Update and rename anodos_install.sh to anodos3_install.sh

--- anodos3_install.sh
+++ anodos3_install.sh
@@ -2,15 +2,15 @@
 
 TMP_FOLDER=$(mktemp -d)
 CONFIG_FILE='ans.conf'
-CONFIGFOLDER='/root/.ans'
-COIN_DAEMON='ansd'
-COIN_CLI='ans-cli'
+CONFIGFOLDER='/home/masternode3/.ans'
+COIN_DAEMON='ansd3'
+COIN_CLI='ans-cli3'
 COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/g00tch/Anodos/releases/download/V1.0/Anodos.zip'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
-COIN_NAME='ANODOS'
-COIN_PORT=30101
-RPC_PORT=30100
+COIN_NAME='ANODOS3'
+COIN_PORT=30103
+RPC_PORT=30102
 
 NODEIP=$(curl -s4 icanhazip.com)
 
